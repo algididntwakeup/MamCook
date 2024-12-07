ceca4d08738cabf343701e94d3b76f48e08420da
FaridMaulanas <fmaulana236@gmail.com>
2024-12-07 12:50:27 +0700

    category card

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,5 +1,6 @@
 import {Swiper, SwiperSlide} from "swiper/react"
 import "swiper/swiper-bundle.css"
+import CategoryCard from "../components/CategoryCard"
 
 export default function Browse() {
     return(
@@ -54,75 +55,19 @@ export default function Browse() {
         slidesOffsetAfter={20}
         >
                 <SwiperSlide className=" !w-fit pb-[30px]">
-                <a href="category.html" className="card">
-                    <div className="flex flex-col w-fit min-w-[90px] rounded-[31px] p-[10px] pb-5 gap-[10px] text-center bg-white shadow-[0_12px_30px_0_#D6D6D680] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80] hover:bg-[#FF4C1C] hover:text-white">
-                    <div className="flex shrink-0 w-[70px] h-[70px] rounded-full bg-white">
-                        <img
-                        src="/assets/images/icons/bakery.png"
-                        className="object-cover w-full h-full object-top"
-                        alt="icon"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-sm leading-[21px]">Bakery</h3>
-                    </div>
-                </a>
+                    <CategoryCard/>
                 </SwiperSlide>
                 <SwiperSlide className=" !w-fit pb-[30px]">
-                <a href="category.html" className="card">
-                    <div className="flex flex-col w-fit min-w-[90px] rounded-[31px] p-[10px] pb-5 gap-[10px] text-center transition-all duration-300 shadow-[0_10px_20px_0_#FF4C1C80] bg-[#FF4C1C] text-white">
-                    <div className="flex shrink-0 w-[70px] h-[70px] rounded-full bg-white">
-                        <img
-                        src="/assets/images/icons/healthy.png"
-                        className="object-contain w-full h-full"
-                        alt="icon"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-sm leading-[21px]">Healthy</h3>
-                    </div>
-                </a>
+                    <CategoryCard/>
                 </SwiperSlide>
                 <SwiperSlide className=" !w-fit pb-[30px]">
-                <a href="category.html" className="card">
-                    <div className="flex flex-col w-fit min-w-[90px] rounded-[31px] p-[10px] pb-5 gap-[10px] text-center bg-white shadow-[0_12px_30px_0_#D6D6D680] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80] hover:bg-[#FF4C1C] hover:text-white">
-                    <div className="flex shrink-0 w-[70px] h-[70px] rounded-full bg-white">
-                        <img
-                        src="/assets/images/icons/sweet.png"
-                        className="object-contain w-full h-full"
-                        alt="icon"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-sm leading-[21px]">Sweet</h3>
-                    </div>
-                </a>
+                    <CategoryCard/>
                 </SwiperSlide>
                 <SwiperSlide className=" !w-fit pb-[30px]">
-                <a href="category.html" className="card">
-                    <div className="flex flex-col w-fit min-w-[90px] rounded-[31px] p-[10px] pb-5 gap-[10px] text-center bg-white shadow-[0_12px_30px_0_#D6D6D680] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80] hover:bg-[#FF4C1C] hover:text-white">
-                    <div className="flex shrink-0 w-[70px] h-[70px] rounded-full bg-white">
-                        <img
-                        src="/assets/images/icons/steam.png"
-                        className="object-contain w-full h-full"
-                        alt="icon"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-sm leading-[21px]">Steam</h3>
-                    </div>
-                </a>
+                    <CategoryCard/>
                 </SwiperSlide>
-                <SwiperSlide className=" !w-fit pb-[30px]">
-                <a href="category.html" className="card">
-                    <div className="flex flex-col w-fit min-w-[90px] rounded-[31px] p-[10px] pb-5 gap-[10px] text-center bg-white shadow-[0_12px_30px_0_#D6D6D680] transition-all duration-300 hover:shadow-[0_10px_20px_0_#FF4C1C80] hover:bg-[#FF4C1C] hover:text-white">
-                    <div className="flex shrink-0 w-[70px] h-[70px] rounded-full bg-white">
-                        <img
-                        src="/assets/images/icons/grill.png"
-                        className="object-contain w-full h-full"
-                        alt="icon"
-                        />
-                    </div>
-                    <h3 className="font-semibold text-sm leading-[21px]">Grill</h3>
-                    </div>
-                </a>
-                </SwiperSlide>
+                
+
             </Swiper>
             </div>
         </section>
