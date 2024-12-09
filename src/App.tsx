8d96ccc2d10f6dc7e13e21aeb1fc5624e18d92d4
FaridMaulanaS <fmaulana236@gmail.com>
2024-12-09 14:20:40 +0700

    buat header kategori resep

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Browse from './pages/Browse';
 // import LoginPage from "./pages/LoginPage";
-
 // import RecipeDetails from './pages/RecipeDetails';
-// import CategoryDetails from './pages/CategoryDetails';
+import CategoryDetails from './pages/CategoryDetails';
 // import SearchDetails from './pages/SearchDetails';
 
 function App() {
@@ -11,10 +10,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Browse />} />
+       
         {/* <Route path="/login" element={<LoginPage />} />? */}
         {/* <Route path="/search" element={<SearchDetails />} />
-        <Route path="/recipe/:slug" element={<RecipeDetails />} />
-        <Route path="/category/:slug" element={<CategoryDetails />} /> */}
+        <Route path="/recipe/:slug" element={<RecipeDetails />} /> */}
+        <Route path="/category/:slug" element={<CategoryDetails />} />
       </Routes>
     </Router>
   );
