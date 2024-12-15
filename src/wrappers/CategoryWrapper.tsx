c4e367a2b6d8bfa5f9fb13f342c3d9a6d509c872
jooofrederik <damare154@gmail.com>
2024-12-15 19:08:31 +0700

    Membuat Folder Types

--- src/wrappers/CategoryWrapper.tsx
+++ src/wrappers/CategoryWrapper.tsx
@@ -1,7 +1,29 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import CategoryCard from "../components/CategoryCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 export default function CategoryWrapper() {
+
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    
+    useEffect(() => {
+      axios.get('')
+        .then(response => {
+          setCategories(response.data.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error);
+          setLoading(false);
+        });
+    }, []);
+    
+
+
     return (
     <section id="Categories" className="mt-[30px]">
             <div className="flex items-center justify-between px-5">
