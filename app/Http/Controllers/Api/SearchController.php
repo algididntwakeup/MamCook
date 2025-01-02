aa049aaea5458f51a2bcba0e3ec5c637cde7cfbf
unknown <rizalalief27@gmail.com>
2025-01-02 21:17:17 +0700

    fix error model relation in api

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -13,8 +13,8 @@ class SearchController extends Controller
     public function index(Request $request) {
         $query = $request->input('query');
 
-        $recipes = Recipe::with('author','LIKE', "%{$query}%")->get();
-        
+        $recipes = Recipe::with('author','LIKE', "%" . $search ."%")->get();
+
         return RecipeResource::collection($recipes);
     }
 }
